36f78312b1e608ea03d0e4c75e8b340f26b3394d
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-02-14 23:26:11 +0000

    Change around models

--- tracker/models.py
+++ tracker/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class PhysicalLocation(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class SeferTorah(models.Model):
     id=models.SmallIntegerField(primary_key=True)
-    physical_location = models.TextField(choices=[
-        ("Main", "Main Shul"),
-        ("SY", "Shaar Yaakov"),
-        ("Horowitz", "Horowitz"),
-        ("YABM", "Yona Avraham")
-        ]
-    )
+    physical_location = models.ForeignKey(PhysicalLocation)
+    picture = models.ImageField()
