5a5fb12bcb84122a2d7ead607ba8de1c6becb994
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-02-14 23:26:19 +0000

    Register models with admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import SeferTorah, PhysicalLocation
+
 # Register your models here.
+admin.site.register(SeferTorah)
+admin.site.register(PhysicalLocation)
+
