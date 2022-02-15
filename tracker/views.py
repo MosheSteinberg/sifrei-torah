7123e477b48b9d3cfe38276d36999b48006dd1bc
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-02-15 15:29:00 +0000

    Minor code clean up

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
 
 from .models import SeferTorah
 
@@ -15,7 +15,5 @@ def SeforimIndex(request):
     return render(request, 'tracker/seforim_index.html', context)
 
 def IndividualSefer(request, id):
-    context = {
-        'sefer': SeferTorah.objects.get(id=id)
-    }
-    return render(request, 'tracker/individual_sefer.html', context)
+    sefer = get_object_or_404(SeferTorah, pk=id)
+    return render(request, 'tracker/individual_sefer.html', {'sefer':sefer})
