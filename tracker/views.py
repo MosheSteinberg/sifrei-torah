294caca0248ebafae757609e130887b028dd1cf4
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-02-15 18:53:49 +0000

    add basic logging in functionality

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from django.contrib.auth.decorators import login_required
+
 from .models import SeferTorah
 
 # Create your views here.
@@ -8,12 +10,14 @@ from .models import SeferTorah
 def index(request):
     return HttpResponse('Hi')
     
+@login_required
 def SeforimIndex(request):
     context = {
         'seforim': SeferTorah.objects.all()
     }
     return render(request, 'tracker/seforim_index.html', context)
 
+@login_required
 def IndividualSefer(request, id):
     sefer = get_object_or_404(SeferTorah, pk=id)
     return render(request, 'tracker/individual_sefer.html', {'sefer':sefer})
