1201c7b7f5257bdc9d431dbed8cf272a08e59d28
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-02-15 14:42:11 +0000

    Add basic list view

--- tracker/views.py
+++ tracker/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import SeferTorah
+
 # Create your views here.
 
 def index(request):
     return HttpResponse('Hi')
     
 def SeforimIndex(request):
-    return HttpResponse('List seforim here')
+    list_of_seforim = [x.id + ": " + x.physical_location for x in SeferTorah.objects.all()]
+    return HttpResponse('<br>'.join(list_of_seforim))
 
 def IndividualSefer(request, id):
+    
     return HttpResponse('Details of sefer %s here.' % id)
