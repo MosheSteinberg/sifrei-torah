b23d4eda636f6adaa43073ba66d98ca1c318d578
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-02-15 18:53:32 +0000

    fix url lookup

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name='tracker'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('seforim', views.SeforimIndex, name='seforim-index'),
