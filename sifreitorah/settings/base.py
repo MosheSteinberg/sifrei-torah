b6e17c06796c7d7f4275801ba93a129508c688a9
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-02-23 14:35:08 +0000

    Further attempt to add logging for server

--- sifreitorah/settings/base.py
+++ sifreitorah/settings/base.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-import django_heroku
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
@@ -156,6 +154,3 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
--- sifreitorah/settings/production.py
+++ sifreitorah/settings/production.py
@@ -11,3 +11,8 @@ try:
     from .local import *
 except ImportError:
     pass
+
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals(), logging=False)
