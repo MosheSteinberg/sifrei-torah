4878b007fab1ecc95f065ef74718934055a43a26
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-03-24 19:40:11 +0000

    Show more in admin view

--- tracker/admin.py
+++ tracker/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 from .models import SeferTorah, PhysicalLocation
 
-# Register your models here.
-admin.site.register(SeferTorah)
-admin.site.register(PhysicalLocation)
 
+class SeferAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'physical_location', 'location_in_text')
+
+# Register your models here.
+admin.site.register(SeferTorah, SeferAdmin)
+admin.site.register(PhysicalLocation)
