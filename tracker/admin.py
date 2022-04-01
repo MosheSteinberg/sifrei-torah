f1afa827dfbf8fdb59c3adccc44676451da1990b
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-04-01 14:34:17 +0100

    Add quality to admin page

--- tracker/admin.py
+++ tracker/admin.py
@@ -4,8 +4,8 @@ from .models import SeferTorah, PhysicalLocation
 
 
 class SeferAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'physical_location', 'location_in_text')
-    list_filter = ('physical_location', 'location_in_text')
+    list_display = ('__str__', 'physical_location', 'location_in_text', 'quality')
+    list_filter = ('physical_location', 'location_in_text', 'quality')
 
 # Register your models here.
 admin.site.register(SeferTorah, SeferAdmin)
